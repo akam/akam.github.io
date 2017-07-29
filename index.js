b66fb20f446a54002a27a83bf0cbd20bdf7ecb3f
akam <albertkam91@gmail.com>
2017-07-28 18:31:36 -0700

    portfolio page version 3

---
+++ index.js
@@ -0,0 +1,79 @@
+var navHeight = $('.nav').outerHeight(true) + 10;
+
+$('.side-bar').affix({
+      offset: {
+        top: navHeight,
+        bottom: navHeight
+      }
+});
+
+
+// $(window).scroll(function() {
+//    var hT = $('#about').offset().top,
+//        hH = $('#about').outerHeight(),
+//        wH = $(window).height(),
+//        wS = $(this).scrollTop();
+//    if (wS > (hT+hH-wH)){
+//        console.log('about on the view!');
+//    }
+// });
+
+// $(window).scroll(function() {
+//    var hT = $('#exp').offset().top,
+//        hH = $('#exp').outerHeight(),
+//        wH = $(window).height(),
+//        wS = $(this).scrollTop();
+//    if (wS > (hT+hH-wH)){
+//        console.log('exp on the view!');
+//    }
+// });
+
+$(window).on('scroll', function() {
+    var y_scroll_pos = window.pageYOffset;
+    var about_pos = $('#about').offset().top;// set to whatever you want it to be
+    var exp_pos = $('#exp').offset().top;
+    var skills_pos = $('#skills').offset().top;
+    var edu_pos = $('#education').offset().top;
+    var proj_pos = $('#projects').offset().top;
+    var contact_pos = $('#contact').offset().top;
+    console.log(skills_pos);
+    console.log(window.pageYOffset);
+    if(y_scroll_pos > about_pos - 200 && y_scroll_pos < about_pos) {
+      toggleSelection('about');
+    }
+    if(y_scroll_pos > exp_pos - 200 && y_scroll_pos < exp_pos) {
+      toggleSelection('exp');
+    }
+    if(y_scroll_pos > skills_pos - 200 && y_scroll_pos < skills_pos) {
+      toggleSelection('skill');
+    }
+    if(y_scroll_pos > edu_pos - 200 && y_scroll_pos < edu_pos) {
+      toggleSelection('edu');
+    }
+    if(y_scroll_pos > proj_pos - 200 && y_scroll_pos < proj_pos) {
+      toggleSelection('project');
+    }
+    if(y_scroll_pos > contact_pos - 200 && y_scroll_pos < contact_pos) {
+      toggleSelection('contact');
+    }
+
+});
+
+$('#click-logo').on('click', () => { toggleSelection('about'); })
+$('#click-about').on('click', () => { toggleSelection('about'); })
+$('#click-edu').on('click', () => { toggleSelection('edu'); })
+$('#click-skill').on('click', () => { toggleSelection('skill'); })
+$('#click-exp').on('click', () => { toggleSelection('exp'); })
+$('#click-project').on('click', () => { toggleSelection('project'); })
+$('#click-contact').on('click', () => { toggleSelection('contact'); })
+
+
+
+function toggleSelection(str){
+  str === 'about' ? $('.about-box').addClass('about-selected') : $('.about-box').removeClass('about-selected');
+  str === 'edu' ? $('.edu-box').addClass('edu-selected') : $('.edu-box').removeClass('edu-selected');
+  str === 'skill' ? $('.skill-box').addClass('skill-selected') : $('.skill-box').removeClass('skill-selected');
+  str === 'exp' ? $('.exp-box').addClass('exp-selected') : $('.exp-box').removeClass('exp-selected');
+  str === 'project' ? $('.project-box').addClass('project-selected') : $('.project-box').removeClass('project-selected');
+  str === 'contact' ? $('.contact-box').addClass('contact-selected') : $('.contact-box').removeClass('contact-selected');
+}
--- script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  var close = document.querySelector(".alert");
-  close.addEventListener("click", function(){
-    close.parentNode.removeChild(close);
-  })
-}
