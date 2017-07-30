1b9954f633b63c97a50fa12d94bd23496b38ffef
akam <albertkam91@gmail.com>
2017-07-30 14:28:19 -0700

    on hover function removed for mobile devices as they were sticky

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
-var navHeight = $('.nav').outerHeight(true) + 10;
+$(document).ready(function(){
 
-$('.side-bar').affix({
-      offset: {
-        top: navHeight,
-        bottom: navHeight
-      }
-});
+  var navHeight = $('.nav').outerHeight(true) + 10;
 
-$(window).on('scroll', function() {
-    var y_scroll_pos = window.pageYOffset;
-    var about_pos = $('#about').offset().top;
-    var exp_pos = $('#exp').offset().top;
-    var skills_pos = $('#skills').offset().top;
-    var edu_pos = $('#education').offset().top;
-    var proj_pos = $('#projects').offset().top;
-    var contact_pos = $('#contact').offset().top;
+  $('.side-bar').affix({
+        offset: {
+          top: navHeight,
+          bottom: navHeight
+        }
+  });
 
-    console.log(skills_pos);
-    console.log(window.pageYOffset);
-    if(y_scroll_pos > about_pos - 200 && y_scroll_pos < about_pos) { toggleSelection('about'); }
-    if(y_scroll_pos > exp_pos - 200 && y_scroll_pos < exp_pos) { toggleSelection('exp'); }
-    if(y_scroll_pos > skills_pos - 200 && y_scroll_pos < skills_pos) { toggleSelection('skill'); }
-    if(y_scroll_pos > edu_pos - 200 && y_scroll_pos < edu_pos) { toggleSelection('edu'); }
-    if(y_scroll_pos > proj_pos - 200 && y_scroll_pos < proj_pos) { toggleSelection('project'); }
-    if(y_scroll_pos > contact_pos - 200 && y_scroll_pos < contact_pos) { toggleSelection('contact'); }
-    if($(window).scrollTop() + $(window).height() == $(document).height()) { toggleSelection('contact'); }
-});
+  $(window).on('scroll', function() {
+      var y_scroll_pos = window.pageYOffset;
+      var about_pos = $('#about').offset().top;
+      var exp_pos = $('#exp').offset().top;
+      var skills_pos = $('#skills').offset().top;
+      var edu_pos = $('#education').offset().top;
+      var proj_pos = $('#projects').offset().top;
+      var contact_pos = $('#contact').offset().top;
 
-$('#click-logo').on('click', () => { toggleSelection('about'); })
-$('#click-about').on('click', () => { toggleSelection('about'); })
-$('#click-edu').on('click', () => { toggleSelection('edu'); })
-$('#click-skill').on('click', () => { toggleSelection('skill'); })
-$('#click-exp').on('click', () => { toggleSelection('exp'); })
-$('#click-project').on('click', () => { toggleSelection('project'); })
-$('#click-contact').on('click', () => { toggleSelection('contact'); })
+      console.log(skills_pos);
+      console.log(window.pageYOffset);
+      if(y_scroll_pos > about_pos - 200 && y_scroll_pos < about_pos) { toggleSelection('about'); }
+      if(y_scroll_pos > exp_pos - 200 && y_scroll_pos < exp_pos) { toggleSelection('exp'); }
+      if(y_scroll_pos > skills_pos - 200 && y_scroll_pos < skills_pos) { toggleSelection('skill'); }
+      if(y_scroll_pos > edu_pos - 200 && y_scroll_pos < edu_pos) { toggleSelection('edu'); }
+      if(y_scroll_pos > proj_pos - 200 && y_scroll_pos < proj_pos) { toggleSelection('project'); }
+      if(y_scroll_pos > contact_pos - 200 && y_scroll_pos < contact_pos) { toggleSelection('contact'); }
+      console.log($(window).scrollTop() + $(window).height(), $(document).height());
+      if($(window).scrollTop() + $(window).height() >= $(document).height() - 1) { toggleSelection('contact'); }
+  });
+
+  $('#click-logo').on('click', () => { toggleSelection('about'); })
+  $('#click-about').on('click', () => { toggleSelection('about'); })
+  $('#click-edu').on('click', () => { toggleSelection('edu'); })
+  $('#click-skill').on('click', () => { toggleSelection('skill'); })
+  $('#click-exp').on('click', () => { toggleSelection('exp'); })
+  $('#click-project').on('click', () => { toggleSelection('project'); })
+  $('#click-contact').on('click', () => { toggleSelection('contact'); })
 
 
 
-function toggleSelection(str){
-  str === 'about' ? $('.about-box').addClass('about-selected') : $('.about-box').removeClass('about-selected');
-  str === 'edu' ? $('.edu-box').addClass('edu-selected') : $('.edu-box').removeClass('edu-selected');
-  str === 'skill' ? $('.skill-box').addClass('skill-selected') : $('.skill-box').removeClass('skill-selected');
-  str === 'exp' ? $('.exp-box').addClass('exp-selected') : $('.exp-box').removeClass('exp-selected');
-  str === 'project' ? $('.project-box').addClass('project-selected') : $('.project-box').removeClass('project-selected');
-  str === 'contact' ? $('.contact-box').addClass('contact-selected') : $('.contact-box').removeClass('contact-selected');
-}
+  function toggleSelection(str){
+    str === 'about' ? $('.about-box').addClass('about-selected') : $('.about-box').removeClass('about-selected');
+    str === 'edu' ? $('.edu-box').addClass('edu-selected') : $('.edu-box').removeClass('edu-selected');
+    str === 'skill' ? $('.skill-box').addClass('skill-selected') : $('.skill-box').removeClass('skill-selected');
+    str === 'exp' ? $('.exp-box').addClass('exp-selected') : $('.exp-box').removeClass('exp-selected');
+    str === 'project' ? $('.project-box').addClass('project-selected') : $('.project-box').removeClass('project-selected');
+    str === 'contact' ? $('.contact-box').addClass('contact-selected') : $('.contact-box').removeClass('contact-selected');
+  }
+})
